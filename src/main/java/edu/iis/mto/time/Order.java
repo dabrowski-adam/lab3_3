1e77bba53e81a08dab81e68a455f0688837118ed
Adam Dąbrowski <190139@edu.p.lodz.pl>
2019-05-04 19:50:33 +0200

    Make use of dependency injection in Order

--- src/main/java/edu/iis/mto/time/Order.java
+++ src/main/java/edu/iis/mto/time/Order.java
@@ -1,5 +1,6 @@
 package edu.iis.mto.time;
 
+import java.time.Clock;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -9,11 +10,13 @@ import org.joda.time.Hours;
 public class Order {
 
     private static final int VALID_PERIOD_HOURS = 24;
+    private final Clock clock;
     private State orderState;
     private List<OrderItem> items = new ArrayList<OrderItem>();
     private DateTime subbmitionDate;
 
-    public Order() {
+    public Order(Clock clock) {
+        this.clock = clock;
         orderState = State.CREATED;
     }
 
@@ -28,12 +31,14 @@ public class Order {
         requireState(State.CREATED);
 
         orderState = State.SUBMITTED;
-        subbmitionDate = new DateTime();
+        subbmitionDate = DateTime.parse(clock.instant().toString());
     }
 
     public void confirm() {
         requireState(State.SUBMITTED);
-        int hoursElapsedAfterSubmittion = Hours.hoursBetween(subbmitionDate, new DateTime())
+
+        DateTime now = DateTime.parse(clock.instant().toString());
+        int hoursElapsedAfterSubmittion = Hours.hoursBetween(subbmitionDate, now)
                                                .getHours();
         if (hoursElapsedAfterSubmittion > VALID_PERIOD_HOURS) {
             orderState = State.CANCELLED;
