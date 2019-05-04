189af6762f865687cdbc2a4eac3568132595f1a4
Adam Dąbrowski <190139@edu.p.lodz.pl>
2019-05-04 20:11:42 +0200

    Check if confirm suceeds for valid order

--- src/test/java/edu/iis/mto/time/OrderTest.java
+++ src/test/java/edu/iis/mto/time/OrderTest.java
@@ -5,6 +5,9 @@ import org.junit.Test;
 import java.time.Instant;
 import java.time.temporal.ChronoUnit;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
 public class OrderTest {
 
     @Test(expected = OrderExpiredException.class)
@@ -17,5 +20,19 @@ public class OrderTest {
         order.submit();
         fakeClock.setInstant(confirmationTime);
         order.confirm();
+    }
+
+    @Test
+    public void confirmShouldSucceedForValidOrder() {
+        Instant submissionTime = Instant.now();
+        Instant confirmationTime = submissionTime.plus(5, ChronoUnit.HOURS);
+
+        FakeClock fakeClock = new FakeClock(submissionTime);
+        Order order = new Order(fakeClock);
+        order.submit();
+        fakeClock.setInstant(confirmationTime);
+        order.confirm();
+
+        assertThat(order.getOrderState(), is(Order.State.SUBMITTED));
     }
 }
